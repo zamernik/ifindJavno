8d95531892d0f49130e4051fc11a1239a37a40a3
zamernik <147094913+zamernik@users.noreply.github.com>
2025-11-29 13:42:36 +0100

    Remove virtual keyword from navigation properties

--- iFind/web/Models/Udelezba.cs
+++ iFind/web/Models/Udelezba.cs
@@ -1,16 +1,16 @@
 #nullable disable
+using System;
+
 namespace web.Models
 {
     public class Udelezba
     {
         public int UporabnikId { get; set; }
-        public virtual Uporabnik Uporabnik { get; set; }
+        public Uporabnik Uporabnik { get; set; }
 
         public int DogodekId { get; set; }
-        public virtual Dogodek Dogodek { get; set; }
+        public Dogodek Dogodek { get; set; }
 
         public DateTime DatumPrijave { get; set; } = DateTime.UtcNow;
-
-        // PK-uporabnikid+dogodekid (OnModelCreating!)
     }
 }
