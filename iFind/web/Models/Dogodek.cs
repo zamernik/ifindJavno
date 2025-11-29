f86b261aa73731053c587c47f2f922f82d8c4965
zamernik <147094913+zamernik@users.noreply.github.com>
2025-11-29 13:36:41 +0100

    Refactor Dogodek class structure

--- iFind/web/Models/Dogodek.cs
+++ iFind/web/Models/Dogodek.cs
@@ -1,28 +1,19 @@
+using System;
+
 namespace web.Models
 {
-public class Dogodek
-{
-    public int Id { get; set; }
-    public string Naziv { get; set; }
-    public string Opis { get; set; }
-    public DateTime DatumCas { get; set; }
+    public class Dogodek
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public DateTime DatumCas { get; set; }
 
-    public string OrganizatorId { get; set; }
+        public string OrganizatorId { get; set; }
 
-    public int KategorijaId { get; set; }
-    public Kategorija Kategorija { get; set; }
+        public int KategorijaId { get; set; }
+        public Kategorija Kategorija { get; set; }
 
-    public Lokacija Lokacija { get; set; }
-}
-
-public class Lokacija
-{
-    public int Id { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public string Naslov { get; set; }
-
-    public int DogodekId { get; set; }
-    public Dogodek Dogodek { get; set; }
-}
+        public Lokacija Lokacija { get; set; }
+    }
 }
