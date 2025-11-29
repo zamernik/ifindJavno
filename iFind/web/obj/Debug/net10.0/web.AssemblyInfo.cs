53c0622f6d33718bada164e4a5b049b6de0a3174
jan2749 <jan.tuhtar7@gmail.com>
2025-11-29 16:15:53 +0100

    popravljen broken zemljevid za vnašanje dogodka

--- iFind/web/obj/Debug/net10.0/web.AssemblyInfo.cs
+++ iFind/web/obj/Debug/net10.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1f09ebdd818a29c6ddee9179cea03047f47321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f8799ff8b9d4b7ab0b6081e2f1bbe60aec1e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
