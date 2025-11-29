c4e81b9b862d14b171d3e2ec5f004d3d35dd027c
zamernik <147094913+zamernik@users.noreply.github.com>
2025-11-29 13:42:21 +0100

    Refactor Lokacija model for property attributes

--- iFind/web/Models/Lokacija.cs
+++ iFind/web/Models/Lokacija.cs
@@ -1,19 +1,22 @@
 #nullable disable
-using System;
 using System.ComponentModel.DataAnnotations;
+
 namespace web.Models
 {
     public class Lokacija
     {
         public int Id { get; set; }
 
-        [Required] public double Latitude { get; set; }
-        [Required] public double Longitude { get; set; }
+        [Required] 
+        public double Latitude { get; set; }
 
-        public string Naslov { get; set; }  //opcijsko za lepši izpis
+        [Required] 
+        public double Longitude { get; set; }
 
-        // Vsaka enemu dogodku (1:1)
+        public string Naslov { get; set; }
+
+        // 1:1 z Dogodek
         public int DogodekId { get; set; }
-        public virtual Dogodek Dogodek { get; set; }
+        public Dogodek Dogodek { get; set; }
     }
 }
