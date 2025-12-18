c0ab99794acca29b9a0ee9b317c4f7867924a130
jan2749 <jan.tuhtar7@gmail.com>
2025-12-18 10:04:19 +0100

    moovable seznam

--- app/src/main/java/com/example/ifindandroid/MainActivity.java
+++ app/src/main/java/com/example/ifindandroid/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         osebe = (TextView) findViewById(R.id.osebe);
+        osebe.setMovementMethod(new ScrollingMovementMethod());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
