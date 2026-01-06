4b5dcb2ae7be80cd4bcdec04f3132916a7406ee5
zamernik <147094913+zamernik@users.noreply.github.com>
2026-01-06 12:34:55 +0100

    Add user to 'Uporabnik' role upon registration

--- iFind/web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ iFind/web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,7 @@ namespace web.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "Uporabnik");
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
